eb03c6ced430a4798c9a6149a545405c58b7e2e9
Gabriel Sanches <g@lonemaker.com>
2018-10-25 13:12:34 -0300

    Use constant to get max uint16

--- close_code.go
+++ close_code.go
@@ -1,4 +1,6 @@
-package wsocket
+package websocket
+
+import "math"
 
 type CloseCode uint16
 
@@ -6,5 +8,5 @@ func (cc CloseCode) isValid() bool {
 	return cc >= 1000 && cc <= 1003 ||
 		cc >= 1007 && cc <= 1011 ||
 		cc >= 3000 && cc <= 5000 ||
-		uint16(cc) == ^uint16(0)
+		int(cc) == math.MaxUint16
 }
